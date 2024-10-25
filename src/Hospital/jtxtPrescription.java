d22cfa87183151b2404a06eee61ca82c3eb35074
Derick Muluka <mulukaderick@gmail.com>
2024-10-25 14:40:32 +0300

    Add Hospital.java file with essential functions for hospital operations

---
+++ src/Hospital/jtxtPrescription.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Hospital;
+
+/**
+ *
+ * @author 25470
+ */
+class jtxtPrescription {
+
+    static void print() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
